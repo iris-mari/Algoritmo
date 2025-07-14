d791ee6af5e9640befd2ae11e3045f8bf3fb5df3
iris-mari <i32882329@gmail.com>
2025-07-14 19:16:57 -0300

    Create operadores.js

---
+++ operadores.js
@@ -0,0 +1,58 @@
+// operadores.js
+
+// 1. Faça a soma de 25 e 17
+let soma = 25 + 17;
+console.log("Soma de 25 e 17:", soma); // 42 
+// Fiz a soma de 25 e 17 onde o resultado é 42
+
+// 2. Subtraia 100 de 250
+let subtracao = 250 - 100;
+console.log("Subtração de 100 de 250:", subtracao); // 150
+// Fiz a subtração entre os números 100 e 250 onde o resultado é 150
+
+// 3. Multiplique 9 por 6
+let multiplicacao = 9 * 6;
+console.log("Multiplicação de 9 por 6:", multiplicacao); // 54
+// Fiz a multiplioação dos números 9 e 6 onde o resultado é 54
+
+// 4. Divida 144 por 12
+let divisao = 144 / 12;
+console.log("Divisão de 144 por 12:", divisao); // 12
+// Dividi o número 144 pelo número 12 onde o resultado é 12
+
+// 5. Calcule o resto da divisão entre 55 e 6
+let resto = 55 % 6;
+console.log("Resto da divisão entre 55 e 6:", resto); // 1
+// Fiz a divisão entre os números 55 e 6 e descobri o resto 1
+
+// 6. Crie uma variável x com valor 10 e incremente uma vez
+let x = 10;
+x++; // incremento
+console.log("Valor de x após incremento:", x); // 11
+// Adicionei o incremento 1 ao número 10 assim, somando (10+1) e achando o número 11
+
+// 7. Crie uma variável y com valor 5 e decremente duas vezes
+let y = 5;
+y--; // primeiro decremento
+y--; // segundo decremento
+console.log("Valor de y após dois decrementos:", y); // 3
+// Retirei dois valores do número 5 assim, subtraindo (5-2) e achando o número 3
+
+// 8. Some 10 com 5, depois subtraia 3 e multiplique por 2
+let resultado = (10 + 5 - 3) * 2;
+console.log("Resultado da expressão:", resultado); // 24
+// Resolvi primeiro os valores de soma e subtração entre os números 10, 5 e 3 dentro do parêntese, e depois multipliquei por 2
+
+// 9. Faça a média de três números: 8, 7.5 e 10
+let media = (8 + 7.5 + 10) / 3;
+console.log("Média de 8, 7.5 e 10:", media); // 8.5
+// Somei os três valores e em seguida, dividi pelo número de elementos, ou seja, 3
+
+// 10. Crie duas variáveis a = 20, b = 5. Use a += b e a *= 2.
+let a = 20;
+let b = 5;
+a += b; // a = a + b = 25
+a *= 2; // a = a * 2 = 50
+console.log("Valor final de a:", a); // 50
+// Na primeira operação eu somei o valor a (20) com o valor b(5) e descori o valor 25
+// Na segunda operação eu multipliquei o valor de a(25) com o número 2
